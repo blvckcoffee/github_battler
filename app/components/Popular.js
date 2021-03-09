d1b3d520f57eb5ac564fa2a59915a78dc9ab2fdf
blvckcoffee <joe.johnson.dev@gmail.com>
2021-03-08 22:09:37 -0600

    Added state for selectedLanguage buttons

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All',
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({ selectedLanguage })
+  }
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'CSS', 'Python']
 
@@ -8,7 +21,17 @@ export default class Popular extends React.Component {
       <ul className='flex-center'>
         {languages.map((language) => (
           <li key={language}>
-            <button className='btn-clear nav-link'>{language}</button>
+            <button
+              className='btn-clear nav-link'
+              style={
+                language === this.state.selectedLanguage
+                  ? { color: 'rgb(235,203,139)' }
+                  : null
+              }
+              onClick={() => this.updateLanguage(language)}
+            >
+              {language}
+            </button>
           </li>
         ))}
       </ul>
