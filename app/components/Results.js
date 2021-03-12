81c4397e08d815d837779b922c3ea9180c56f013
blvckcoffee <joe.johnson.dev@gmail.com>
2021-03-12 14:50:14 -0600

    Used queryString to move player state to URL, allow for reset and changing query strings in URL

--- app/components/Results.js
+++ app/components/Results.js
@@ -12,6 +12,8 @@ import Card from './Card'
 import PropTypes from 'prop-types'
 import Loading from './Loading'
 import Tooltip from './Tooltip'
+import queryString from 'query-string'
+import { Link } from 'react-router-dom'
 
 const styles = {
   container: {
@@ -86,7 +88,9 @@ export default class Results extends React.Component {
     }
   }
   componentDidMount() {
-    const { playerOne, playerTwo } = this.props
+    const { playerOne, playerTwo } = queryString.parse(
+      this.props.location.search
+    )
 
     battle([playerOne, playerTwo])
       .then((players) => {
@@ -136,16 +140,10 @@ export default class Results extends React.Component {
             <ProfileList profile={loser.profile} />
           </Card>
         </div>
-        <button className='btn dark-btn btn-space' onClick={this.props.onReset}>
+        <Link className='btn dark-btn btn-space' to='/battle'>
           Reset
-        </button>
+        </Link>
       </React.Fragment>
     )
   }
-}
-
-Results.propTypes = {
-  playerOne: PropTypes.string.isRequired,
-  playerTwo: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired,
 }
--- app/index.js
+++ app/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
-import { ThemeProvider } from './contexts/theme'
+import Results from './components/Results'
 import Nav from './components/Nav'
+import { ThemeProvider } from './contexts/theme'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
@@ -29,7 +30,8 @@ class App extends React.Component {
               <Nav />
 
               <Route exact path='/' component={Popular} />
-              <Route path='/battle' component={Battle} />
+              <Route exact path='/battle' component={Battle} />
+              <Route path='/battle/results' component={Results} />
             </div>
           </div>
         </ThemeProvider>
