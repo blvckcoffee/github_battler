ebf260ae427a3e4105317e5a8b39e1d0a869c915
blvckcoffee <joe.johnson.dev@gmail.com>
2021-03-12 14:10:49 -0600

    Added routes for views with react router, edited webpack.config for history api fallback

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
+    publicPath: '/',
   },
 
   module: {
@@ -20,4 +21,7 @@ module.exports = {
       template: 'app/index.html',
     }),
   ],
+  devServer: {
+    historyApiFallback: true,
+  },
 }
